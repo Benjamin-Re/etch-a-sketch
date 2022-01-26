5aa887e86bd1c181b96f6a553b0e11e23c823e9c
Benjamin-Re <renker.benjamin@gmail.com>
2022-01-26 11:10:18 +0100

    Add gray scale function

--- script.js
+++ script.js
@@ -1,20 +1,92 @@
+// fields
 const container = document.querySelector(".container");
-// Add a grid of 16x16 square divs to container
-let numItems = 16;
-for(let i = 0; i < numItems**2; i++) {
-    let newDiv = document.createElement("div");
-    let size = 800/numItems;
-    newDiv.style.cssText = `background-color: red; width: ${size}px; height: ${size}px`;
-    container.appendChild(newDiv); 
-}
-// Change the div color on hover
-// Get node list of all divs
-let divs = document.querySelectorAll(".container>div");
+let rainbow = false;
+let gray = false;
 
-// Loop through the node list and add an event listener to each div
-divs.forEach(div => div.addEventListener("mouseover", changeColor));
+
+
+function createGrid(numItems) {
+    if(numItems === undefined){
+        numItems = 16;
+    }
+    for (let i = 0; i < numItems ** 2; i++) {
+        let newDiv = document.createElement("div");
+        let size = 800 / numItems;
+        newDiv.style.cssText = `background-color: red; width: ${size}px; height: ${size}px`;
+        container.appendChild(newDiv);
+    }
+    // Change the div color on hover
+    // Get node list of all divs
+    let divs = document.querySelectorAll(".container>div");
+
+    // Loop through the node list and add an event listener to each div
+    // Create a counter to keep track of the number of events
+    divs.forEach(div => div.addEventListener("mouseover", changeColor));
+    // Give each div a counter attribute
+    divs.forEach(div => div.setAttribute("data-count", 0));
+}
+
+
 
 // The function for the event listener adds a new class to the div
-function changeColor(e){
-    e.target.style.backgroundColor = "blue";
+function changeColor(e) {
+    let color = "blue";
+    if(rainbow){
+        let red = Math.floor(Math.random()*255)+1;
+        let green = Math.floor(Math.random()*255)+1;
+        let blue = Math.floor(Math.random()*255)+1;
+        color = `rgb(${red},${green},${blue})`;
+    } else if (gray){
+        let currentData = e.target.getAttribute("data-count");
+        if(currentData < 10){
+            e.target.setAttribute("data-count", +currentData+1);
+        }
+        color = `rgb(0, 0, 0, ${currentData/10})`;
+        console.log(e.target.getAttribute("data-count"));
+ 
+    }
+    e.target.style.backgroundColor = color;
 }
+
+// Configure button to clear the current grid & ask user for number of rows
+const clearBtn = document.querySelector(".clear");
+clearBtn.addEventListener("click", clear);
+function clear() {
+    // Delete the old grid
+    container.textContent="";
+    // Ask for number of cells
+    numItems = prompt("How many cells per side do you need?");
+    // Create a new grid in the same space
+    createGrid(numItems);
+}
+
+// Configure rainbow button to draw rainbow colors
+// Get the btn
+const rainbowBtn = document.querySelector(".rainbow");
+rainbowBtn.addEventListener("click", drawRainbow);
+
+function drawRainbow(){
+    if(rainbow){
+        rainbow=false;
+    } else {
+        rainbow = true;
+        gray = false;
+    }
+    
+}
+
+// Get gray scale button
+const grayBtn = document.querySelector(".gray");
+grayBtn.addEventListener("click", drawGray);
+
+function drawGray(){
+    if(gray){
+        gray=false;
+    } else {
+        gray = true;
+        rainbow = false;
+    }
+}
+
+// Create the grid on page load
+createGrid();
